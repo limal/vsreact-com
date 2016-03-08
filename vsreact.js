5d04926d5aa40290a2375bf3e3d7db32dc841d53
Lukasz Wolnik <lukasz@wolnik.co.uk>
2016-03-08 14:23:45 +0000

    Added Webpack support for React

--- vsreact.js
+++ vsreact.js
@@ -1,1 +1,5 @@
-require('./scss/main.scss');
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Hello from './js/components/Hello.jsx';
+
+ReactDOM.render(<Hello />, document.getElementById('app'));
--- webpack.config.js
+++ webpack.config.js
@@ -2,12 +2,26 @@ module.exports = {
 	entry: "./vsreact.js",
 	output: {
 		path: __dirname,
-		filename: "./js/vsreact.bundle.js"
+		filename: "./vsreact.bundle.js"
 	},
 	module: {
 		loaders: [
-			{ test: /\.scss$/, loaders: ["style", "css", "sass"] },
-			{ test: /.(png|woff(2)?|eot|ttf|svg)(\?[a-z0-9=\.]+)?$/, loader: 'url-loader?limit=100000' }
+			{
+				test: /\.scss$/,
+				loaders: ["style", "css", "sass"]
+			},
+			{
+				test: /.(png|woff(2)?|eot|ttf|svg)(\?[a-z0-9=\.]+)?$/,
+				loader: 'url-loader?limit=100000'
+			},
+			{
+				test: /\.jsx?$/,
+				exclude: /node_modules/,
+				loader: 'babel',
+				query: {
+					presets: ['react', 'es2015']
+				}
+			}
 		]
 	}
 };
